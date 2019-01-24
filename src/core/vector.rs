811ab4236228f8d226a9cd3921232572b0589da4
ian <ianmccall93@gmail.com>
2019-01-23 18:02:04 -0800

    AddAssign, SubAssign, MulAssign, DivAssign for Vector2

--- src/core/vector.rs
+++ src/core/vector.rs
@@ -2,6 +2,10 @@ use std::ops::Add;
 use std::ops::Sub;
 use std::ops::Mul;
 use std::ops::Div;
+use std::ops::AddAssign;
+use std::ops::SubAssign;
+use std::ops::MulAssign;
+use std::ops::DivAssign;
 use std::marker::Sized;
 use crate::core::scalar::Scalar;
 
@@ -13,6 +17,10 @@ pub trait Vector
     + Sub<Vector2, Output = Self>
     + Mul<Scalar, Output = Self>
     + Div<Scalar, Output = Self>
+    + AddAssign<Vector2>
+    + SubAssign<Vector2>
+    + MulAssign<Scalar>
+    + DivAssign<Scalar>
 {
     fn zero() -> Self;
     fn dot(self, rhs: Self) -> Scalar;
@@ -29,6 +37,7 @@ pub trait Vector
         self / self.length()
     }
 }
+
 
 #[derive(Clone, Copy)]
 pub struct Vector2 {
@@ -57,10 +66,44 @@ impl Mul<Scalar> for Vector2 {
     }
 }
 
+// Unfortunately not possible to do this more generally for all vectors, see link:
+// https://users.rust-lang.org/t/implementing-generic-trait-with-local-struct-on-local-trait/23225
+impl Mul<Vector2> for Scalar {
+    type Output = Vector2;
+    fn mul(self, rhs: Vector2) -> Vector2 {
+        rhs * self
+    }
+}
+
+
 impl Div<Scalar> for Vector2 {
     type Output = Self;
     fn div(self, rhs: Scalar) -> Self {
         Vector2 {x: self.x / rhs, y: self.y / rhs}
+    }
+}
+
+impl AddAssign for Vector2 {
+    fn add_assign(&mut self, rhs: Self){
+        *self = *self + rhs;
+    }
+}
+
+impl SubAssign for Vector2 {
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = *self - rhs;
+    }
+}
+
+impl MulAssign<Scalar> for Vector2 {
+    fn mul_assign(&mut self, rhs: Scalar) {
+        *self = *self * rhs;
+    }
+}
+
+impl DivAssign<Scalar> for Vector2 {
+    fn div_assign(&mut self, rhs: Scalar) {
+        *self = *self / rhs;
     }
 }
 
