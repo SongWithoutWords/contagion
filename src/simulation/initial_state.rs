ac401de9b345b4058a464d6e2d50cdf52247bd34
ian <ianmccall93@gmail.com>
2019-01-25 17:28:06 -0800

    Fix-up simulation initial state

--- src/simulation/initial_state.rs
+++ src/simulation/initial_state.rs
@@ -7,7 +7,10 @@ use crate::constants::presentation::*;
 
 use super::state::*;
 
-pub fn init_human(count: u32, state: &mut State) {
+pub fn initial_state(count: u32) -> State {
+
+    let mut state = State { entities: vec!()};
+
     let entities = &mut state.entities;
 
     for _i in 0..count {
@@ -15,12 +18,11 @@ pub fn init_human(count: u32, state: &mut State) {
         let mut rng = rand::thread_rng();
         let x: Scalar = rng.gen_range(0.0f64, WINDOW_W as f64);
         let y: Scalar = rng.gen_range(0.0f64, WINDOW_H as f64);
-        let position = Vector2::zero();
-        position.set_x(x);
-        position.set_y(y);
+        let position = Vector2{x: x, y: y};
         let velocity = Vector2::zero();
         entities.push(generate_human(position, velocity));
     }
+    state
 }
 
 pub fn generate_human(position: Vector2, velocity: Vector2) -> Entity {
