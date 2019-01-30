396420c2c0398ba1addd6b9ab368fc9a3d221b60
John So <jungwookso@gmail.com>
2019-01-30 15:44:03 -0800

    distribution of diff entities to count

--- src/simulation/initial_state.rs
+++ src/simulation/initial_state.rs
@@ -20,7 +20,21 @@ pub fn initial_state(count: u32) -> State {
         let y: Scalar = rng.gen_range(0.0f64, 10 as f64); //WINDOW_H as f64);
         let position = Vector2{x: x, y: y};
         let velocity = Vector2::zero();
-        entities.push(Entity{position, velocity, behaviour: Behaviour::Human});
+        let human_count: u32 = (count as f32 * 0.8) as u32;
+        let cop_count: u32 = (count as f32 * 0.18) as u32;
+        let zombie_count = count - (human_count + cop_count);
+        // spawn 80% humans
+        if _i < human_count {
+            entities.push(Entity { position, velocity, behaviour: Behaviour::Human });
+        }
+        // spawn 18% cops
+        else if  _i >= human_count && _i < (count - zombie_count) {
+            entities.push(Entity { position, velocity, behaviour: Behaviour::Cop });
+        }
+        // spawn rest zombie
+        else {
+            entities.push(Entity { position, velocity, behaviour: Behaviour::Zombie });
+        }
     }
     state
 }
