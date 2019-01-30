80599e45861cd2b7b83dfe75f511bff1733d1c35
John So <jungwookso@gmail.com>
2019-01-30 00:41:47 -0800

    quick change to elapsed name

--- src/main.rs
+++ src/main.rs
@@ -98,9 +98,9 @@ fn main() {
     while running {
         // Handle FPS
         {
-            // use elapsed for transforming matrices
+            // use elapsed_t for transforming matrices
             let dt = last_frame.elapsed().subsec_nanos() as f32 / 1.0e6; // ns -> ms
-            elapsed = dt / 1.0e3; // ms -> s
+            elapsed_t = dt / 1.0e3; // ms -> s
             last_frame = Instant::now();
             fps += 1;
             if last_frame.duration_since(last_second).as_secs() >= 1 {
