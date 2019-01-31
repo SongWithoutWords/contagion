6435ad77335398b9b240b8595792d14fedfc5a49
John So <jungwookso@gmail.com>
2019-01-30 16:01:48 -0800

    moved to outer loop

--- src/simulation/initial_state.rs
+++ src/simulation/initial_state.rs
@@ -8,7 +8,9 @@ use crate::constants::presentation::*;
 use super::state::*;
 
 pub fn initial_state(count: u32) -> State {
-
+    let human_count: u32 = (count as f32 * 0.8) as u32;
+    let cop_count: u32 = (count as f32 * 0.18) as u32;
+    let zombie_count = count - (human_count + cop_count);
     let mut state = State { entities: vec!()};
 
     let entities = &mut state.entities;
@@ -20,9 +22,6 @@ pub fn initial_state(count: u32) -> State {
         let y: Scalar = rng.gen_range(0.0f64, 10 as f64); //WINDOW_H as f64);
         let position = Vector2{x: x, y: y};
         let velocity = Vector2::zero();
-        let human_count: u32 = (count as f32 * 0.8) as u32;
-        let cop_count: u32 = (count as f32 * 0.18) as u32;
-        let zombie_count = count - (human_count + cop_count);
         // spawn 80% humans
         if _i < human_count {
             entities.push(Entity { position, velocity, behaviour: Behaviour::Human });
