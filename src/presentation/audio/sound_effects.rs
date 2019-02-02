ef812b58ac72b1cd099f973f993b04200a03e230
ian <ianmccall93@gmail.com>
2019-02-02 14:22:16 -0800

    Add missing module from last commit :p

---
+++ src/presentation/audio/sound_effects.rs
@@ -0,0 +1,11 @@
+use crate::simulation::update::SoundEffect;
+
+pub fn play_sound_effects(sounds: &Vec<SoundEffect>) {
+
+    for sound in sounds {
+        match sound {
+            // TODO
+            _ => ()
+        }
+    }
+}
