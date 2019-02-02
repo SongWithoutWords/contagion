2b1b7c9268c6b2bed80756793651b397925ac8af
ian <ianmccall93@gmail.com>
2019-02-01 23:00:36 -0800

    Minor tidy of inital state

--- src/simulation/initial_state.rs
+++ src/simulation/initial_state.rs
@@ -11,16 +11,16 @@ pub fn initial_state(count: u32) -> State {
     let human_count: u32 = (count as f32 * 0.8) as u32;
     let cop_count: u32 = (count as f32 * 0.18) as u32;
     let zombie_count = count - (human_count + cop_count);
-    let mut state = State { entities: vec!()};
+    let mut state = State { entities: vec!() };
 
     let entities = &mut state.entities;
 
     for _i in 0..count {
         // TODO: need to optimize this later with housing units and two entities shouldn't be placed on same tile
         let mut rng = rand::thread_rng();
-        let x: Scalar = rng.gen_range(0.0f64, 10 as f64); //WINDOW_W as f64);
-        let y: Scalar = rng.gen_range(0.0f64, 10 as f64); //WINDOW_H as f64);
-        let position = Vector2{x: x, y: y};
+        let x: Scalar = rng.gen_range(0.0f64, 10 as f64);
+        let y: Scalar = rng.gen_range(0.0f64, 10 as f64);
+        let position = vector2(x, y);
         let velocity = Vector2::zero();
         // spawn 80% humans
         if _i < human_count {
