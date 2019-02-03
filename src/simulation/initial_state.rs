d56bb902fbc1db836a76c0a5d40197fbd0fc6eee
kitty7756 <kitty7756@hotmail.com>
2019-02-02 22:02:22 -0800

    changed back initial state

--- src/simulation/initial_state.rs
+++ src/simulation/initial_state.rs
@@ -15,27 +15,25 @@ pub fn initial_state(count: u32) -> State {
 
     let entities = &mut state.entities;
 
-    entities.push(Entity { position: Vector2{x: 0.0, y: 0.0}, velocity: Vector2::zero(), behaviour: Behaviour::Cop { waypoint: None}});
-
-//    for _i in 0..count {
-//        // TODO: need to optimize this later with housing units and two entities shouldn't be placed on same tile
-//        let mut rng = rand::thread_rng();
-//        let x: Scalar = rng.gen_range(0.0f64, 10 as f64); //WINDOW_W as f64);
-//        let y: Scalar = rng.gen_range(0.0f64, 10 as f64); //WINDOW_H as f64);
-//        let position = Vector2{x: x, y: y};
-//        let velocity = Vector2::zero();
-//        // spawn 80% humans
-//        if _i < human_count {
-//            entities.push(Entity { position, velocity, behaviour: Behaviour::Human });
-//        }
-//        // spawn 18% cops
-//        if  _i >= human_count && _i < (count - zombie_count) {
-//            entities.push(Entity { position, velocity, behaviour: Behaviour::Cop });
-//        }
-//        // spawn rest zombie
-//        else {
-//            entities.push(Entity { position, velocity, behaviour: Behaviour::Zombie });
-//        }
-//    }
+    for _i in 0..count {
+        // TODO: need to optimize this later with housing units and two entities shouldn't be placed on same tile
+        let mut rng = rand::thread_rng();
+        let x: Scalar = rng.gen_range(0.0f64, 10 as f64); //WINDOW_W as f64);
+        let y: Scalar = rng.gen_range(0.0f64, 10 as f64); //WINDOW_H as f64);
+        let position = Vector2{x: x, y: y};
+        let velocity = Vector2::zero();
+        // spawn 80% humans
+        if _i < human_count {
+            entities.push(Entity { position, velocity, behaviour: Behaviour::Human });
+        }
+        // spawn 18% cops
+        if  _i >= human_count && _i < (count - zombie_count) {
+            entities.push(Entity { position: Vector2{x: 0.0, y: 0.0}, velocity: Vector2::zero(), behaviour: Behaviour::Cop { waypoint: None}} );
+        }
+        // spawn rest zombie
+        else {
+            entities.push(Entity { position, velocity, behaviour: Behaviour::Zombie });
+        }
+    }
     state
 }
