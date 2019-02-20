b78a656f8c32a21541259bf49368e1b6e93a0d91
Cate Mackenzie <cate.c.mackenzie@gmail.com>
2019-02-20 13:49:42 -0800

    Adds ability to render colored, textureless shapes, draws buildings (assume all are rectangles for now)

--- src/presentation/display.rs
+++ src/presentation/display.rs
@@ -1,5 +1,6 @@
 use crate::core::vector::*;
 use crate::core::matrix::*;
+use crate::core::geo::polygon::*;
 use crate::simulation::state::*;
 
 use glium::Surface;
@@ -37,6 +38,7 @@ pub struct Programs {
     sprite_program: glium::Program,
     shadow_program: glium::Program,
     gui_program: glium::Program,
+    shape_program: glium::Program
 }
 pub fn load_programs(window: &glium_sdl2::SDL2Facade) -> Programs {
     Programs {
@@ -52,6 +54,10 @@ pub fn load_programs(window: &glium_sdl2::SDL2Facade) -> Programs {
             window,
             include_str!("graphics/shadow.vs.glsl"),
             include_str!("graphics/shadow.fs.glsl"), None).unwrap(),
+        shape_program: glium::Program::from_source(
+            window,
+            include_str!("graphics/shape.vs.glsl"),
+            include_str!("graphics/shape.fs.glsl"), None).unwrap(),
     }
 }
 
@@ -62,6 +68,13 @@ struct Vertex {
 }
 implement_vertex!(Vertex, position, tex_coords);
 
+#[derive(Copy, Clone)]
+struct ColorVertex {
+    position: [f32; 2],
+    tex_coords: [f32; 2],
+    color: [f32; 3],
+}
+implement_vertex!(ColorVertex, position, tex_coords, color);
 
 fn push_sprite_vertices(buffer: &mut Vec<Vertex>, entity: &Entity) {
 
@@ -146,10 +159,62 @@ fn push_gui_vertices(buffer: &mut Vec<Vertex>, ui: &Gui) {
     buffer.push(vertex2);
 }
 
+// TODO: Assumes building is a rectangle and not an arbitrary polygon, consider generalizing
+fn push_building_vertices(buffer: &mut Vec<ColorVertex>, building: &Polygon, color: [f32; 3]) {
+    let top_left = building.get(0);
+    let top_right = building.get(1);
+    let bot_right = building.get(2);
+    let bot_left = building.get(3);
+
+    let vertex0 = ColorVertex {
+        position: top_left.as_f32_array(),
+        tex_coords: [0.0, 1.0],
+        color
+    };
+    let vertex1 = ColorVertex {
+        position: top_right.as_f32_array(),
+        tex_coords: [1.0, 1.0],
+        color
+    };
+    let vertex2 = ColorVertex {
+        position: bot_left.as_f32_array(),
+        tex_coords: [0.0, 0.0],
+        color
+    };
+    let vertex3 = ColorVertex {
+        position: bot_right.as_f32_array(),
+        tex_coords: [1.0, 0.0],
+        color
+    };
+    buffer.push(vertex0);
+    buffer.push(vertex1);
+    buffer.push(vertex2);
+    buffer.push(vertex1);
+    buffer.push(vertex3);
+    buffer.push(vertex2);
+}
+
 fn draw_sprites<U>(
     frame: &mut glium::Frame,
     window: &glium_sdl2::SDL2Facade,
     vertices: &Vec<Vertex>,
+    program: &glium::Program,
+    params: &glium::DrawParameters,
+    uniforms: &U)
+    where U: glium::uniforms::Uniforms
+{
+    frame.draw(
+        &glium::VertexBuffer::new(window, vertices).unwrap(),
+        &glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList),
+        program,
+        uniforms,
+        params).unwrap();
+}
+
+fn draw_color_sprites<U>(
+    frame: &mut glium::Frame,
+    window: &glium_sdl2::SDL2Facade,
+    vertices: &Vec<ColorVertex>,
     program: &glium::Program,
     params: &glium::DrawParameters,
     uniforms: &U)
@@ -178,6 +243,7 @@ pub fn display(
 
     let mut vertex_buffers = enum_map!{_ => vec!()};
     let mut vertex_buffers_gui = enum_map!{_ => vec!()};
+    let mut vertex_buffers_building = vec!();
 
     // Compute the vertices in world coordinates of all entities
     for entity in &state.entities {
@@ -189,7 +255,6 @@ pub fn display(
         };
         push_sprite_vertices(&mut vertex_buffers[sprite_type], entity);
     }
-
 
     // Compute vertices for selection highlights
     {
@@ -204,6 +269,12 @@ pub fn display(
                 push_gui_vertices(&mut vertex_buffers_gui[GuiType::Selected], ui);
             }
         }
+    }
+
+    // Compute vertices for buildings
+    for building in &state.buildings {
+        let color = [0.1, 0.1, 0.1];
+        push_building_vertices(&mut vertex_buffers_building, building, color);
     }
 
     let camera_frame = camera_frame.as_f32_array();
@@ -238,6 +309,19 @@ pub fn display(
             window,
             &vertex_buffer,
             &programs.sprite_program,
+            params,
+            &uniforms);
+    }
+
+    {
+        let uniforms = uniform! {
+            matrix: camera_frame
+        };
+        draw_color_sprites(
+            frame,
+            window,
+            &vertex_buffers_building,
+            &programs.shape_program,
             params,
             &uniforms);
     }
