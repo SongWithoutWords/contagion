767ae2e74e421ff928d42b462b8286632537a79b
Behar Hyseni <behar.hyseni@alumni.ubc.ca>
2019-02-20 19:45:23 -0800

    fixed merge conflicts

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,27 @@
-#[macro_use]
-extern crate enum_map;
-#[macro_use]
-extern crate glium;
-extern crate glium_sdl2;
-extern crate image;
-extern crate music;
-extern crate rand;
-extern crate rand_xorshift;
-extern crate sdl2;
-
-use std::fs::File;
-use std::time::Instant;
-
-use glium::draw_parameters::Blend;
-use glium_sdl2::SDL2Facade;
-use sdl2::{EventPump, Sdl};
-use sdl2::keyboard::Keycode;
-
-use crate::core::scalar::*;
-use crate::core::vector::*;
-use crate::presentation::ui::glium_text;
-
 pub mod constants;
 pub mod core;
 pub mod presentation;
 pub mod simulation;
+
+#[macro_use] extern crate glium;
+extern crate glium_sdl2;
+extern crate sdl2;
+#[macro_use] extern crate enum_map;
+extern crate image;
+extern crate rand;
+extern crate rand_xorshift;
+extern crate music;
+
+use sdl2::{Sdl, EventPump};
+use sdl2::keyboard::Keycode;
+use std::fs::File;
+use std::time::Instant;
+use glium_sdl2::SDL2Facade;
+use glium::draw_parameters::Blend;
+
+use crate::core::scalar::*;
+use crate::core::vector::*;
+use crate::presentation::ui::glium_text;
 
 #[derive(Copy, Clone, Hash, PartialEq, Eq)]
 enum Music {
@@ -45,7 +42,6 @@ fn init() -> Result<((Sdl, SDL2Facade, EventPump),
                      glium_text::FontTexture),
     String> {
 
-
     // initialize window and eventpump
     let window_tuple = presentation::graphics::renderer::create_window();
     let window = window_tuple.1;
@@ -63,6 +59,7 @@ fn init() -> Result<((Sdl, SDL2Facade, EventPump),
 
     Ok((window_tuple, textures, programs, font))
 }
+
 
 // Plays the shotgun sound once every time it is called
 pub fn play_shotgun() {
@@ -83,6 +80,7 @@ pub fn play_reload() {
 pub fn play_zombie_dead() {
     music::play_sound(&TheSound::ZombieDeath, music::Repeat::Times(1), music::MAX_VOLUME);
 }
+
 
 
 fn main() {
@@ -106,10 +104,6 @@ fn main() {
     };
 
     let mut state = simulation::initial_state::initial_state(100, rand::random::<u32>());
-
-    let mut ui = presentation::ui::gui::Gui::new(presentation::ui::gui::GuiType::Selected, 0.1, 0.1, Vector2 { x: -0.9, y: -0.9 });
-    let mut camera = presentation::camera::Camera::new();
-
     let mut component = presentation::ui::gui::Component::init_demo();
     let mut camera = presentation::camera::Camera::new();
 
@@ -135,82 +129,68 @@ fn main() {
 
 
         // main game loop
-        'main_game_loop: loop {
+    'main_game_loop: loop {
+        // Compute delta time
+        let duration = last_frame.elapsed();
+        let delta_time = duration.as_secs() as Scalar + 1e-9 * duration.subsec_nanos() as Scalar;
+        last_frame = Instant::now();
+        let keyboard_state = event_pump.keyboard_state();
 
+        camera.update(&keyboard_state, delta_time);
 
-            // Compute delta time
-            let duration = last_frame.elapsed();
-            let delta_time = duration.as_secs() as Scalar + 1e-9 * duration.subsec_nanos() as Scalar;
-            last_frame = Instant::now();
-            let keyboard_state = event_pump.keyboard_state();
+        let camera_frame = camera.compute_matrix();
 
-            camera.update(&keyboard_state, delta_time);
-
-            let camera_frame = camera.compute_matrix();
-
-            // Event loop: polls for events sent to all windows
-            for event in event_pump.poll_iter() {
-                use sdl2::event::Event;
-                match event {
-                    // Exit window if escape key pressed or quit event triggered
-                    Event::Quit { .. } | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                        break 'main_game_loop;
-                    }
-                    Event::KeyDown { keycode: Some(Keycode::P), .. } => {
-                        game_paused = !game_paused;
-                    }
-                    Event::KeyDown { keycode: Some(Keycode::L), .. } => {
-                        println!("Debug info:");
-                        println!("  DT:               {:?}", delta_time);
-                        println!("  FPS:              {:?}", 1.0 / delta_time);
-                        println!("  Entity count:     {:?}", state.entities.len());
-                        println!("  Projectile count: {:?}", state.projectiles.len());
-                    }
-                    Event::MouseButtonDown { timestamp: _, window_id: _, which: _, mouse_btn, x, y } => {
-                        use sdl2::mouse::MouseButton;
-                        match mouse_btn {
-                            MouseButton::Left { .. } => {
-                                simulation::control::update_selected(0, &mut state, &window, camera_frame, x, y);
-                                for i in 0..state.is_selected.len() {
-
-                                    if state.is_selected[i] == true {
-
-                                    if state.is_selected[i] {
-
-                                        println!("selected: {:?}", state.is_selected[i]);
-                                    }
+        // Event loop: polls for events sent to all windows
+        for event in event_pump.poll_iter() {
+            use sdl2::event::Event;
+            match event {
+                // Exit window if escape key pressed or quit event triggered
+                Event::Quit { .. } | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'main_game_loop;
+                },
+                Event::KeyDown { keycode: Some(Keycode::P), .. } => {
+                    game_paused = ! game_paused;
+                },
+                Event::KeyDown { keycode: Some(Keycode::L), .. } => {
+                    println!("Debug info:");
+                    println!("  DT:               {:?}", delta_time);
+                    println!("  FPS:              {:?}", 1.0 / delta_time);
+                    println!("  Entity count:     {:?}", state.entities.len());
+                    println!("  Projectile count: {:?}", state.projectiles.len());
+                },
+                Event::MouseButtonDown {timestamp: _, window_id: _, which: _, mouse_btn, x, y } => {
+                    use sdl2::mouse::MouseButton;
+                    match mouse_btn {
+                        MouseButton::Left { .. } => {
+                            simulation::control::update_selected(0, &mut state, &window, camera_frame, x, y);
+                            for i in 0..state.is_selected.len() {
+                                if state.is_selected[i] {
+                                    println!("selected: {:?}", state.is_selected[i]);
                                 }
                             }
-                            MouseButton::Right { .. } => {
-                                simulation::control::issue_police_order(simulation::control::PoliceOrder::Move, &mut state, &window, camera_frame, x, y);
-                            }
-                            _ => ()
                         }
+                        MouseButton::Right { .. } => {
+                            simulation::control::issue_police_order(simulation::control::PoliceOrder::Move, &mut state, &window, camera_frame, x, y);
+                        }
+                        _ => ()
                     }
-                    Event::MouseWheel { timestamp: _, window_id: _, which: _, x: _, y, direction: _ } => {
-                        camera.set_zoom(y);
-                    }
-                    _ => ()
+                },
+                Event::MouseWheel {timestamp: _, window_id: _, which: _, x: _, y, direction: _} => {
+                    camera.set_zoom(y);
                 }
+                _ => ()
             }
-
-            if !game_paused {
-                let _not_paused_game = simulation::update::update(
-                    &simulation::update::UpdateArgs { dt: delta_time },
-                    &mut state);
-            }
-
-
-            let mut target = window.draw();
-            presentation::display::display(&mut target, &window, &programs, &textures, &params, &state, camera_frame, &mut ui, &font);
-            target.finish().unwrap();
         }
-    });
-}
+
+        if !game_paused {
+            let _not_paused_game = simulation::update::update(
+                &simulation::update::UpdateArgs { dt: delta_time },
+                &mut state);
+        }
 
         let mut target = window.draw();
         presentation::display::display(&mut target, &window, &programs, &textures, &params, &state, camera_frame, &mut component, &font);
         target.finish().unwrap();
     }
+});
 }
-
