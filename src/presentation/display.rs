87fc6ababe13aa3e1b48bfb762accd29c390f490
Behar Hyseni <behar.hyseni@alumni.ubc.ca>
2019-03-07 00:30:51 -0800

    implemented the civilian UI icon (+ shows current number)

--- src/presentation/display.rs
+++ src/presentation/display.rs
@@ -441,7 +441,10 @@ pub fn display(
             GuiType::ZombieUI => {
                 push_gui_vertices(&mut vertex_buffers_gui[SpriteType::ZombieWorldIcon], component);
             },
-                        GuiType::Selected => {
+            GuiType::CivilianUI => {
+                push_gui_vertices(&mut vertex_buffers_gui[SpriteType::CivilianWorldIcon], component);
+            },
+            GuiType::Selected => {
                 if selection_count < 1 {
 
                 } else {
@@ -666,6 +669,21 @@ pub fn display(
                 params,
                 &uniforms);
         }
+          else if _gui_type == SpriteType::CivilianWorldIcon {
+            let uniforms = uniform! {
+                    matrix: mat_gui,
+                    tex: &textures.sprite_textures[_gui_type],
+                };
+            // Draw the text showing the number of cops next to the UI cop icon
+            draw_remaining_civilian_num(window, human_count, frame, &font);
+            draw_color_sprites(
+                frame,
+                window,
+                &vertex_buffer,
+                &programs.sprite_program,
+                params,
+                &uniforms);
+        }
 
 
 
@@ -750,6 +768,31 @@ fn draw_remaining_zombie_num(window: &glium_sdl2::SDL2Facade, zombie_num: i32, f
         [0.0, 1.0 * (w as f32) / (h as f32) / font_scale_down,0.0, 0.0],
         [0.0, 0.0, 1.0, 0.0],
         [0.8755, 0.83, 0.0, 1.0f32],
+    ];
+
+    glium_text::draw(&text, &system, frame, matrix, color);
+
+}
+
+// Draw the remaining number of civilians in the world (number)
+fn draw_remaining_civilian_num(window: &glium_sdl2::SDL2Facade, civilian_num: i32, frame: &mut glium::Frame, font: &FontTexture){
+    let system = glium_text::TextSystem::new(window);
+    let civilian_num_str: String =  civilian_num.to_string();
+    let mut civilian_num_display = format!("0{}",  civilian_num_str);
+    if civilian_num > 99 {
+        civilian_num_display = format!("{}",  civilian_num_str);
+    }
+    let str_slice: &str = &civilian_num_display[..];
+    let text = glium_text::TextDisplay::new(&system, font, str_slice);
+    let color = [0.0, 0.0, 0.0, 1.0f32];
+    let font_scale_down = 40.0;
+    let (w, h) = frame.get_dimensions();
+
+    let matrix = [
+        [1.0/font_scale_down, 0.0, 0.0, 0.0],
+        [0.0, 1.0 * (w as f32) / (h as f32) / font_scale_down,0.0, 0.0],
+        [0.0, 0.0, 1.0, 0.0],
+        [0.7255, 0.83, 0.0, 1.0f32],
     ];
 
     glium_text::draw(&text, &system, frame, matrix, color);
